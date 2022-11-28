7931c56f8c7dad5e2cb86ecd00e42ce7f43ebf7d
Haofei Xu <xuhfgm@gmail.com>
2022-11-28 17:10:34 +0800

    fix typo

--- pip_install.sh
+++ pip_install.sh
@@ -3,4 +3,4 @@
 
 pip install torch==1.9.0+cu102 torchvision==0.10.0+cu102 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 
-pip install imageio=2.9.0 imageio-ffmpeg matplotlib opencv-python pillow scikit-image scipy tensorboard==2.9.1 setuptools==59.5.0
+pip install imageio==2.9.0 imageio-ffmpeg matplotlib opencv-python pillow scikit-image scipy tensorboard==2.9.1 setuptools==59.5.0
